f3f3fd22ac0c648fd4534641f02c2fc9278d8994
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2020-06-08 15:19:44 -0400

    Adding enviornment variables

--- buildAmi.sh
+++ buildAmi.sh
@@ -1,4 +1,6 @@
 ./packer build \
+    -var  'aws_access_key=$aws_access_key'
+    -var  'aws_secret_key=$aws_secret_key'
     -var 'aws_region=us-east-1' \
     -var 'subnet_id=subnet-09c00b56' \
     -var 'ami_description=AMI for Assignment 4 using packer' \
